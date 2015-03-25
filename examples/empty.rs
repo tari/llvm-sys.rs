51e1cfa255bc4017a7f4f48ce11def9112051446
Peter Marheine <peter@taricorp.net>
2015-03-24 23:13:27 -0600

    Improve docs and example a bit.

--- examples/empty.rs
+++
@@ -1,9 +0,0 @@
-extern crate "llvm-sys" as llvm;
-
-fn main() {
-    unsafe {
-        let module = llvm::core::LLVMModuleCreateWithName(b"empty\0".as_ptr() as *const _);
-        llvm::core::LLVMDumpModule(module);
-        llvm::core::LLVMDisposeModule(module);
-    }
-}
