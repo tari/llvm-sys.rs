ce8d186430c51c5adbbbb01a72bc04523cddbba7
Peter Marheine <peter@taricorp.net>
2015-03-26 12:47:44 -0600

    Transforms.

---
+++ src/transforms/vectorize.rs
@@ -0,0 +1,9 @@
+//! Vectorization transformations of LLVM IR.
+
+use super::super::prelude::*;
+
+extern "C" {
+    pub fn LLVMAddBBVectorizePass(PM: LLVMPassManagerRef) -> ();
+    pub fn LLVMAddLoopVectorizePass(PM: LLVMPassManagerRef) -> ();
+    pub fn LLVMAddSLPVectorizePass(PM: LLVMPassManagerRef) -> ();
+}
