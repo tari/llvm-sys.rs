e1bbc4240efe3f8b08408ef798ec1403c8bb4d13
Peter Marheine <peter@taricorp.net>
2015-03-26 11:20:40 -0600

    More headers, shuffle some defs around to their real headers.

---
+++ src/target.rs
@@ -0,0 +1,130 @@
+use super::prelude::*;
+
+#[repr(C)]
+pub enum LLVMByteOrdering {
+    LLVMBigEndian = 0,
+    LLVMLittleEndian = 1
+}
+
+#[repr(C)]
+pub struct LLVMOpaqueTargetData;
+pub type LLVMTargetDataRef = *mut LLVMOpaqueTargetData;
+
+#[repr(C)]
+pub struct LLVMOpaqueTargetLibraryInfotData;
+pub type LLVMTargetLibraryInfoRef = *mut LLVMOpaqueTargetLibraryInfotData;
+
+extern "C" {
+    pub fn LLVMInitializeR600TargetInfo() -> ();
+    pub fn LLVMInitializeSystemZTargetInfo() -> ();
+    pub fn LLVMInitializeHexagonTargetInfo() -> ();
+    pub fn LLVMInitializeNVPTXTargetInfo() -> ();
+    pub fn LLVMInitializeCppBackendTargetInfo() -> ();
+    pub fn LLVMInitializeMSP430TargetInfo() -> ();
+    pub fn LLVMInitializeXCoreTargetInfo() -> ();
+    pub fn LLVMInitializeMipsTargetInfo() -> ();
+    pub fn LLVMInitializeAArch64TargetInfo() -> ();
+    pub fn LLVMInitializeARMTargetInfo() -> ();
+    pub fn LLVMInitializePowerPCTargetInfo() -> ();
+    pub fn LLVMInitializeSparcTargetInfo() -> ();
+    pub fn LLVMInitializeX86TargetInfo() -> ();
+    pub fn LLVMInitializeR600Target() -> ();
+    pub fn LLVMInitializeSystemZTarget() -> ();
+    pub fn LLVMInitializeHexagonTarget() -> ();
+    pub fn LLVMInitializeNVPTXTarget() -> ();
+    pub fn LLVMInitializeCppBackendTarget() -> ();
+    pub fn LLVMInitializeMSP430Target() -> ();
+    pub fn LLVMInitializeXCoreTarget() -> ();
+    pub fn LLVMInitializeMipsTarget() -> ();
+    pub fn LLVMInitializeAArch64Target() -> ();
+    pub fn LLVMInitializeARMTarget() -> ();
+    pub fn LLVMInitializePowerPCTarget() -> ();
+    pub fn LLVMInitializeSparcTarget() -> ();
+    pub fn LLVMInitializeX86Target() -> ();
+    pub fn LLVMInitializeR600TargetMC() -> ();
+    pub fn LLVMInitializeSystemZTargetMC() -> ();
+    pub fn LLVMInitializeHexagonTargetMC() -> ();
+    pub fn LLVMInitializeNVPTXTargetMC() -> ();
+    pub fn LLVMInitializeCppBackendTargetMC() -> ();
+    pub fn LLVMInitializeMSP430TargetMC() -> ();
+    pub fn LLVMInitializeXCoreTargetMC() -> ();
+    pub fn LLVMInitializeMipsTargetMC() -> ();
+    pub fn LLVMInitializeAArch64TargetMC() -> ();
+    pub fn LLVMInitializeARMTargetMC() -> ();
+    pub fn LLVMInitializePowerPCTargetMC() -> ();
+    pub fn LLVMInitializeSparcTargetMC() -> ();
+    pub fn LLVMInitializeX86TargetMC() -> ();
+    pub fn LLVMInitializeR600AsmPrinter() -> ();
+    pub fn LLVMInitializeSystemZAsmPrinter() -> ();
+    pub fn LLVMInitializeHexagonAsmPrinter() -> ();
+    pub fn LLVMInitializeNVPTXAsmPrinter() -> ();
+    pub fn LLVMInitializeMSP430AsmPrinter() -> ();
+    pub fn LLVMInitializeXCoreAsmPrinter() -> ();
+    pub fn LLVMInitializeMipsAsmPrinter() -> ();
+    pub fn LLVMInitializeAArch64AsmPrinter() -> ();
+    pub fn LLVMInitializeARMAsmPrinter() -> ();
+    pub fn LLVMInitializePowerPCAsmPrinter() -> ();
+    pub fn LLVMInitializeSparcAsmPrinter() -> ();
+    pub fn LLVMInitializeX86AsmPrinter() -> ();
+    pub fn LLVMInitializeR600AsmParser() -> ();
+    pub fn LLVMInitializeSystemZAsmParser() -> ();
+    pub fn LLVMInitializeMipsAsmParser() -> ();
+    pub fn LLVMInitializeAArch64AsmParser() -> ();
+    pub fn LLVMInitializeARMAsmParser() -> ();
+    pub fn LLVMInitializePowerPCAsmParser() -> ();
+    pub fn LLVMInitializeSparcAsmParser() -> ();
+    pub fn LLVMInitializeX86AsmParser() -> ();
+    pub fn LLVMInitializeSystemZDisassembler() -> ();
+    pub fn LLVMInitializeHexagonDisassembler() -> ();
+    pub fn LLVMInitializeXCoreDisassembler() -> ();
+    pub fn LLVMInitializeMipsDisassembler() -> ();
+    pub fn LLVMInitializeAArch64Disassembler() -> ();
+    pub fn LLVMInitializeARMDisassembler() -> ();
+    pub fn LLVMInitializePowerPCDisassembler() -> ();
+    pub fn LLVMInitializeSparcDisassembler() -> ();
+    pub fn LLVMInitializeX86Disassembler() -> ();
+}
+
+extern "C" {
+    pub fn LLVMCreateTargetData(StringRep: *const ::libc::c_char)
+     -> LLVMTargetDataRef;
+    pub fn LLVMAddTargetData(TD: LLVMTargetDataRef, PM: LLVMPassManagerRef)
+     -> ();
+    pub fn LLVMAddTargetLibraryInfo(TLI: LLVMTargetLibraryInfoRef,
+                                    PM: LLVMPassManagerRef) -> ();
+    pub fn LLVMCopyStringRepOfTargetData(TD: LLVMTargetDataRef)
+     -> *mut ::libc::c_char;
+    pub fn LLVMByteOrder(TD: LLVMTargetDataRef) -> LLVMByteOrdering;
+    pub fn LLVMPointerSize(TD: LLVMTargetDataRef) -> ::libc::c_uint;
+    pub fn LLVMPointerSizeForAS(TD: LLVMTargetDataRef, AS: ::libc::c_uint)
+     -> ::libc::c_uint;
+    pub fn LLVMIntPtrType(TD: LLVMTargetDataRef) -> LLVMTypeRef;
+    pub fn LLVMIntPtrTypeForAS(TD: LLVMTargetDataRef, AS: ::libc::c_uint)
+     -> LLVMTypeRef;
+    pub fn LLVMIntPtrTypeInContext(C: LLVMContextRef, TD: LLVMTargetDataRef)
+     -> LLVMTypeRef;
+    pub fn LLVMIntPtrTypeForASInContext(C: LLVMContextRef,
+                                        TD: LLVMTargetDataRef,
+                                        AS: ::libc::c_uint) -> LLVMTypeRef;
+    pub fn LLVMSizeOfTypeInBits(TD: LLVMTargetDataRef, Ty: LLVMTypeRef)
+     -> ::libc::c_ulonglong;
+    pub fn LLVMStoreSizeOfType(TD: LLVMTargetDataRef, Ty: LLVMTypeRef)
+     -> ::libc::c_ulonglong;
+    pub fn LLVMABISizeOfType(TD: LLVMTargetDataRef, Ty: LLVMTypeRef)
+     -> ::libc::c_ulonglong;
+    pub fn LLVMABIAlignmentOfType(TD: LLVMTargetDataRef, Ty: LLVMTypeRef)
+     -> ::libc::c_uint;
+    pub fn LLVMCallFrameAlignmentOfType(TD: LLVMTargetDataRef,
+                                        Ty: LLVMTypeRef) -> ::libc::c_uint;
+    pub fn LLVMPreferredAlignmentOfType(TD: LLVMTargetDataRef,
+                                        Ty: LLVMTypeRef) -> ::libc::c_uint;
+    pub fn LLVMPreferredAlignmentOfGlobal(TD: LLVMTargetDataRef,
+                                          GlobalVar: LLVMValueRef)
+     -> ::libc::c_uint;
+    pub fn LLVMElementAtOffset(TD: LLVMTargetDataRef, StructTy: LLVMTypeRef,
+                               Offset: ::libc::c_ulonglong) -> ::libc::c_uint;
+    pub fn LLVMOffsetOfElement(TD: LLVMTargetDataRef, StructTy: LLVMTypeRef,
+                               Element: ::libc::c_uint)
+     -> ::libc::c_ulonglong;
+    pub fn LLVMDisposeTargetData(TD: LLVMTargetDataRef) -> ();
+}
---
+++ src/target_machine.rs
@@ -0,0 +1,98 @@
+use super::prelude::*;
+use super::target::LLVMTargetDataRef;
+
+#[repr(C)]
+pub struct LLVMOpaqueTargetMachine;
+pub type LLVMTargetMachineRef = *mut LLVMOpaqueTargetMachine;
+
+#[repr(C)]
+pub struct LLVMTarget;
+pub type LLVMTargetRef = *mut LLVMTarget;
+
+#[repr(C)]
+pub enum LLVMCodeGenOptLevel {
+    LLVMCodeGenLevelNone = 0,
+    LLVMCodeGenLevelLess = 1,
+    LLVMCodeGenLevelDefault = 2,
+    LLVMCodeGenLevelAggressive = 3
+}
+
+#[repr(C)]
+pub enum LLVMRelocMode {
+    LLVMRelocDefault = 0,
+    LLVMRelocStatic = 1,
+    LLVMRelocPIC = 2,
+    LLVMRelocDynamicNoPic = 3,
+}
+
+#[repr(C)]
+#[derive(Copy)]
+pub enum LLVMCodeModel {
+    LLVMCodeModelDefault = 0,
+    LLVMCodeModelJITDefault = 1,
+    LLVMCodeModelSmall = 2,
+    LLVMCodeModelKernel = 3,
+    LLVMCodeModelMedium = 4,
+    LLVMCodeModelLarge = 5,
+}
+
+#[repr(C)]
+pub enum LLVMCodeGenFileType {
+    LLVMAssemblyFile = 0,
+    LLVMObjectFile = 1,
+}
+
+extern "C" {
+    pub fn LLVMGetFirstTarget() -> LLVMTargetRef;
+    pub fn LLVMGetNextTarget(T: LLVMTargetRef) -> LLVMTargetRef;
+    pub fn LLVMGetTargetFromName(Name: *const ::libc::c_char)
+     -> LLVMTargetRef;
+    pub fn LLVMGetTargetFromTriple(Triple: *const ::libc::c_char,
+                                   T: *mut LLVMTargetRef,
+                                   ErrorMessage: *mut *mut ::libc::c_char)
+     -> LLVMBool;
+    pub fn LLVMGetTargetName(T: LLVMTargetRef) -> *const ::libc::c_char;
+    pub fn LLVMGetTargetDescription(T: LLVMTargetRef)
+     -> *const ::libc::c_char;
+    pub fn LLVMTargetHasJIT(T: LLVMTargetRef) -> LLVMBool;
+    pub fn LLVMTargetHasTargetMachine(T: LLVMTargetRef) -> LLVMBool;
+    pub fn LLVMTargetHasAsmBackend(T: LLVMTargetRef) -> LLVMBool;
+    pub fn LLVMCreateTargetMachine(T: LLVMTargetRef,
+                                   Triple: *const ::libc::c_char,
+                                   CPU: *const ::libc::c_char,
+                                   Features: *const ::libc::c_char,
+                                   Level: LLVMCodeGenOptLevel,
+                                   Reloc: LLVMRelocMode,
+                                   CodeModel: LLVMCodeModel)
+     -> LLVMTargetMachineRef;
+    pub fn LLVMDisposeTargetMachine(T: LLVMTargetMachineRef) -> ();
+    pub fn LLVMGetTargetMachineTarget(T: LLVMTargetMachineRef)
+     -> LLVMTargetRef;
+    pub fn LLVMGetTargetMachineTriple(T: LLVMTargetMachineRef)
+     -> *mut ::libc::c_char;
+    pub fn LLVMGetTargetMachineCPU(T: LLVMTargetMachineRef)
+     -> *mut ::libc::c_char;
+    pub fn LLVMGetTargetMachineFeatureString(T: LLVMTargetMachineRef)
+     -> *mut ::libc::c_char;
+    pub fn LLVMGetTargetMachineData(T: LLVMTargetMachineRef)
+     -> LLVMTargetDataRef;
+    pub fn LLVMSetTargetMachineAsmVerbosity(T: LLVMTargetMachineRef,
+                                            VerboseAsm: LLVMBool) -> ();
+    pub fn LLVMTargetMachineEmitToFile(T: LLVMTargetMachineRef,
+                                       M: LLVMModuleRef,
+                                       Filename: *mut ::libc::c_char,
+                                       codegen: LLVMCodeGenFileType,
+                                       ErrorMessage: *mut *mut ::libc::c_char)
+     -> LLVMBool;
+    pub fn LLVMTargetMachineEmitToMemoryBuffer(T: LLVMTargetMachineRef,
+                                               M: LLVMModuleRef,
+                                               codegen: LLVMCodeGenFileType,
+                                               ErrorMessage:
+                                                   *mut *mut ::libc::c_char,
+                                               OutMemBuf:
+                                                   *mut LLVMMemoryBufferRef)
+     -> LLVMBool;
+    pub fn LLVMGetDefaultTargetTriple() -> *mut ::libc::c_char;
+    pub fn LLVMAddAnalysisPasses(T: LLVMTargetMachineRef,
+                                 PM: LLVMPassManagerRef) -> ();
+}
