7e745e8a0002468f9cc78a74f8771c50dac13210
Peter Marheine <peter@taricorp.net>
2016-09-18 15:29:13 +1000

    First steps to version pinning
    Introduces a warning when the detected version of LLVM doesn't match the
    crate version (supported since Cargo 0.11) and removes the hard failure
    when LLVM less than 3.6 is detected.

--- build.rs
+++ build.rs
@@ -1,9 +1,10 @@
 extern crate gcc;
+#[macro_use] extern crate lazy_static;
 extern crate regex;
 extern crate semver;
 
 use regex::Regex;
-use semver::{Version, VersionReq};
+use semver::Version;
 use std::process::Command;
 
 /// Get the output from running `llvm-config` with the given argument.
@@ -29,15 +30,36 @@ fn llvm_version() -> Version {
     Version::parse(&version_str[start..end]).unwrap()
 }
 
+lazy_static! {
+    static ref CRATE_VERSION: Version = Version::parse(env!("CARGO_PKG_VERSION"))
+        .expect("Crate version is somehow not valid semver");
+}
+
 fn main() {
-    // Check for LLVM 3.6 or greater.
-    let minimum_llvm_version = VersionReq::parse(">=3.6").unwrap();
-    let version = llvm_version();
-    if minimum_llvm_version.matches(&version) {
-        println!("Found LLVM version {}", version);
-    } else {
-        panic!("LLVM version 3.6 or higher is required. (Found {})", version);
+    let target_ver = Version {
+        major: CRATE_VERSION.major / 10,
+        minor: CRATE_VERSION.major % 10,
+        patch: 0, pre: vec![], build: vec![]
     };
+    let llvm_ver = llvm_version();
+
+    // Check discovered LLVM version and warn if it does not match
+    // the crate version.
+    // We'd prefer to make version mismatches a hard error because we can't
+    // reasonably detect incompatiblities here, but since setting up LLVM is
+    // non-trivial prefer to warn. The API is largely stable across versions
+    // and they don't change the semantics of existing functions (fortunately),
+    // so errors should be caught at link-time.
+    //
+    // In the future we'd like to provide an option to compile a version of LLVM
+    // precisely matching what the crate expects so these kinds of problems can
+    // be worked around with relative ease.
+    if target_ver.major != llvm_ver.major || target_ver.minor != llvm_ver.minor {
+        println!("cargo:warning={} version {} expects LLVM {}, but found {}",
+                 env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"), target_ver, llvm_ver);
+        println!("cargo:warning=  Depending on the API features in use, required functions may not exist.");
+        println!("cargo:warning=  If you observe LLVM-related linker errors, this is probably the cause.");
+    }
 
     // Parse library linking flags from llvm-config.
     for arg in llvm_config("--ldflags").split_whitespace() {
