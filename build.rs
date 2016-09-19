8dcf48978ad34e27208cb3eef7b3385d7e7b6ea0
Peter Marheine <peter@taricorp.net>
2016-09-19 20:49:13 +1000

    Add support for env-var-based custom LLVM paths with versioning

--- build.rs
+++ build.rs
@@ -5,62 +5,151 @@ extern crate semver;
 
 use regex::Regex;
 use semver::Version;
+use std::env;
+use std::ffi::OsStr;
+use std::io::{self, ErrorKind};
+use std::path::PathBuf;
 use std::process::Command;
 
+lazy_static!{
+    /// LLVM version used by this version of the crate.
+    static ref CRATE_VERSION: Version = {
+        let crate_version = Version::parse(env!("CARGO_PKG_VERSION"))
+            .expect("Crate version is somehow not valid semver");
+        Version {
+            major: crate_version.major / 10,
+            minor: crate_version.major % 10,
+            .. crate_version
+        }
+    };
+
+    /// Filesystem path to an llvm-config binary for the correct version.
+    static ref LLVM_CONFIG_PATH: PathBuf = {
+        // Try llvm-config via PATH first.
+        if let Some(name) = locate_system_llvm_config() {
+            return name.into();
+        } else {
+            println!("Didn't find usable system-wide LLVM.");
+        }
+        
+        // Did the user give us a binary path to use? If yes, try
+        // to use that and fail if it doesn't work.
+        let binary_prefix_var = format!("LLVM_SYS_{}_PREFIX",
+                                        env!("CARGO_PKG_VERSION_MAJOR"));
+        if let Some(path) = env::var_os(&binary_prefix_var) {
+            let mut pb: PathBuf = path.into();
+            pb.push("bin");
+            pb.push("llvm-config");
+
+            let ver = llvm_version(&pb)
+                .expect(&format!("Failed to execute {:?}", &pb));
+            if is_compatible_llvm(&ver) {
+                return pb;
+            } else {
+                panic!("LLVM binaries specified by {} are the wrong version.
+                        (Found {}, need {}.)", binary_prefix_var, ver, *CRATE_VERSION);
+            }
+        } else {
+            println!("{} not set, not using precompiled binaries", binary_prefix_var);
+        }
+
+        // No binaries. Offer to download and compile a blessed version,
+        // but only with positive confirmation. It's a fairly large download,
+        // takes a while and could get wiped out by a `cargo clean` so it's
+        // useful if doing an install but is in general not recommended.
+        let autobuild_var = format!("LLVM_SYS_{}_AUTOBUILD",
+                                    env!("CARGO_PKG_VERSION_MAJOR"));
+        match env::var_os(&autobuild_var) {
+            Some(ref x) => {
+                if x == "YES" {
+                    unimplemented!();
+                } else {
+                    println!("{} must be exactly \"YES\" to enable autobuild (is {:?})",
+                             autobuild_var, x);
+                }
+            }
+            None => {
+                println!("{} not set, will not automatically compile LLVM",
+                         autobuild_var);
+            }
+        }
+
+        panic!("Could not find a compatible version of LLVM");
+    };
+}
+
+/// Try to find a system-wide version of llvm-config that is compatible with
+/// this crate.
+///
+/// Returns None on failure.
+fn locate_system_llvm_config() -> Option<&'static str> {
+    match llvm_version("llvm-config") {
+        Ok(ref version) if is_compatible_llvm(version) => {
+            // Compatible version found. Nice.
+            return Some("llvm-config");
+        },
+        Ok(version) => {
+            // Version mismatch. Will try further searches, but warn that
+            // we're not using the system one.
+            println!("Found LLVM version {} on PATH, but need {}.",
+                     version, *CRATE_VERSION);
+        },
+        Err(ref e) if e.kind() == ErrorKind::NotFound => {
+            // Looks like we failed to execute any llvm-config. Keep
+            // searching.
+        },
+        // Some other error, probably a weird failure. Give up.
+        Err(e) => panic!("Failed to search PATH for llvm-config: {}", e),
+    }
+
+    None
+}
+
+/// Check whether the given LLVM version is compatible with this version of
+/// the crate.
+fn is_compatible_llvm(llvm_version: &Version) -> bool {
+    llvm_version.major == CRATE_VERSION.major &&
+        llvm_version.minor == CRATE_VERSION.minor
+}
+
 /// Get the output from running `llvm-config` with the given argument.
+///
+/// Lazily searches for or compiles LLVM as configured by the environment
+/// variables.
 fn llvm_config(arg: &str) -> String {
-    let stdout = Command::new("llvm-config")
+    llvm_config_ex(&*LLVM_CONFIG_PATH, arg)
+        .expect("Surprising failure from llvm-config")
+}
+
+/// Invoke the specified binary as llvm-config.
+///
+/// Explicit version of the `llvm_config` function that bubbles errors
+/// up.
+fn llvm_config_ex<S: AsRef<OsStr>>(binary: S, arg: &str)
+        -> io::Result<String> {
+    Command::new(binary)
         .arg(arg)
         .output()
-        .expect("Couldn't execute llvm-config")
-        .stdout;
-
-    String::from_utf8(stdout).expect("llvm-config output was not UTF-8.")
+        .map(|output| String::from_utf8(output.stdout)
+            .expect("Output from llvm-config was not valid UTF-8"))
 }
 
 /// Get the LLVM version using llvm-config.
-fn llvm_version() -> Version {
-    let version_str = llvm_config("--version");
+fn llvm_version<S: AsRef<OsStr>>(binary: S) -> io::Result<Version> {
+    let version_str = try!(llvm_config_ex(binary.as_ref(), "--version"));
 
+    // LLVM isn't really semver and uses version suffixes to build
+    // version strings like '3.8.0svn', so limit what we try to parse
+    // to only the numeric bits.
     let re = Regex::new(r"^(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)")
         .unwrap();
     let (start, end) = re.find(&version_str)
         .expect("Could not determine LLVM version from llvm-config.");
 
-    Version::parse(&version_str[start..end]).unwrap()
-}
-
-lazy_static! {
-    static ref CRATE_VERSION: Version = Version::parse(env!("CARGO_PKG_VERSION"))
-        .expect("Crate version is somehow not valid semver");
+    Ok(Version::parse(&version_str[start..end]).unwrap())
 }
 
 fn main() {
-    let target_ver = Version {
-        major: CRATE_VERSION.major / 10,
-        minor: CRATE_VERSION.major % 10,
-        patch: 0, pre: vec![], build: vec![]
-    };
-    let llvm_ver = llvm_version();
-
-    // Check discovered LLVM version and warn if it does not match
-    // the crate version.
-    // We'd prefer to make version mismatches a hard error because we can't
-    // reasonably detect incompatiblities here, but since setting up LLVM is
-    // non-trivial prefer to warn. The API is largely stable across versions
-    // and they don't change the semantics of existing functions (fortunately),
-    // so errors should be caught at link-time.
-    //
-    // In the future we'd like to provide an option to compile a version of LLVM
-    // precisely matching what the crate expects so these kinds of problems can
-    // be worked around with relative ease.
-    if target_ver.major != llvm_ver.major || target_ver.minor != llvm_ver.minor {
-        println!("cargo:warning={} version {} expects LLVM {}, but found {}",
-                 env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"), target_ver, llvm_ver);
-        println!("cargo:warning=  Depending on the API features in use, required functions may not exist.");
-        println!("cargo:warning=  If you observe LLVM-related linker errors, this is probably the cause.");
-    }
-
     // Parse library linking flags from llvm-config.
     for arg in llvm_config("--ldflags").split_whitespace() {
         if arg.starts_with("-L") {
