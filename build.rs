fe181a95b9469ac1e65c14d490762333e79552c7
Peter Marheine <peter@taricorp.net>
2017-07-24 11:14:24 +1000

    Clean -W... flags from LLVM CFLAGS at build time.
    Workaround for issue #17. Can be disabled by setting
    `LLVM_SYS_<version>_NO_CLEAN_CFLAGS` at build-time.
    (grafted from 76d2b51d974ebdbcf064c4457a85b8c258b096af)

--- build.rs
+++ build.rs
@@ -194,6 +194,29 @@ fn llvm_version<S: AsRef<OsStr>>(binary: S) -> io::Result<Version> {
     Ok(Version::parse(&version_str[start..end]).unwrap())
 }
 
+fn get_llvm_cflags() -> String {
+    let output = llvm_config("--cflags");
+
+    // llvm-config includes cflags from its own compilation with --cflags that
+    // may not be relevant to us. In particularly annoying cases, these might
+    // include flags that aren't understood by the default compiler we're
+    // using. Unless requested otherwise, clean CFLAGS of options that are
+    // known to be possibly-harmful.
+    let no_clean = env::var_os(format!("LLVM_SYS_{}_NO_CLEAN_CFLAGS",
+                                       env!("CARGO_PKG_VERSION_MAJOR"))).is_some();
+    if no_clean || cfg!(target_env = "msvc") {
+        // MSVC doesn't accept -W... options, so don't try to strip them and
+        // possibly strip something that should be retained. Also do nothing if
+        // the user requests it.
+        return output;
+    }
+
+    llvm_config("--cflags").split(&[' ', '\n'][..])
+        .filter(|word| !word.starts_with("-W"))
+        .collect::<Vec<_>>()
+        .join(" ")
+}
+
 fn main() {
     // Parse library linking flags from llvm-config.
     for arg in llvm_config("--ldflags").split_whitespace() {
@@ -240,6 +263,6 @@ fn main() {
     }
 
     // Build the extra wrapper functions.
-    std::env::set_var("CFLAGS", llvm_config("--cflags").trim());
+    std::env::set_var("CFLAGS", get_llvm_cflags());
     gcc::compile_library("libtargetwrappers.a", &["wrappers/target.c"]);
 }
