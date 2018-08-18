41b4a4630b71efc0d2faca6d09a9fe71b006634d
Peter Marheine <peter@taricorp.net>
2018-08-19 09:54:42 +1000

    Correct empty lines in multiline println
    The previous commit removed leading spaces from otherwise blank lines, which
    changes the interpretation of the string literal to include the leading spaces
    because they're inconsistent. Put them back so the output remains unindented.

--- build.rs
+++ build.rs
@@ -53,10 +53,10 @@ lazy_static!{
 
         println!("No suitable version of LLVM was found system-wide or pointed
                   to by {}.
-
+                  
                   Consider using `llvmenv` to compile an appropriate copy of LLVM, and
                   refer to the llvm-sys documentation for more information.
-
+                  
                   llvm-sys: https://crates.io/crates/llvm-sys
                   llvmenv: https://crates.io/crates/llvmenv", binary_prefix_var);
         panic!("Could not find a compatible version of LLVM");
