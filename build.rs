5637452d516dbdbd74d0f85b44df155d70975f02
Sam Rijs <srijs@airpost.net>
2018-12-22 21:44:21 +1100

    Build wrappers even if no-llvm-linking is enabled

--- build.rs
+++ build.rs
@@ -279,6 +279,12 @@ fn get_llvm_cflags() -> String {
 }
 
 fn main() {
+    // Build the extra wrapper functions.
+    std::env::set_var("CFLAGS", get_llvm_cflags());
+    cc::Build::new()
+        .file("wrappers/target.c")
+        .compile("targetwrappers");
+
     if cfg!(feature = "no-llvm-linking") {
         return;
     }
@@ -308,10 +314,4 @@ fn main() {
     if force_ffi {
         println!("cargo:rustc-link-lib=dylib={}", "ffi");
     }
-
-    // Build the extra wrapper functions.
-    std::env::set_var("CFLAGS", get_llvm_cflags());
-    cc::Build::new()
-        .file("wrappers/target.c")
-        .compile("targetwrappers");
 }
