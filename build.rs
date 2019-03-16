0e3c6d19efdf19eaf5a4bbd1d8d3c17b43ecfbf0
Andrew Hu <andrew98034@gmail.com>
2019-03-16 01:43:57 -0700

    link appropriate debug libraries on Windows (grafted from 8d937987925a10cf7d37eedc9ae75d8feef1b3fd)

--- build.rs
+++ build.rs
@@ -278,6 +278,11 @@ fn get_llvm_cflags() -> String {
         .join(" ")
 }
 
+fn is_llvm_debug() -> bool {
+    // Has to be either Debug or Release
+    llvm_config("--build-mode").contains("Debug")
+}
+
 fn main() {
     // Build the extra wrapper functions.
     if !cfg!(feature = "disable-alltargets-init") {
@@ -304,6 +309,10 @@ fn main() {
     // Link system libraries
     for name in get_system_libraries() {
         println!("cargo:rustc-link-lib=dylib={}", name);
+    }
+    
+    if is_llvm_debug() && cfg!(target_env = "msvc") {
+        println!("cargo:rustc-link-lib={}", "msvcrtd");
     }
 
     // Link libffi if the user requested this workaround.
