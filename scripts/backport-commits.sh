9234f3ef17f0cb66152c91af25c30033c023a74a
Peter Marheine <peter@taricorp.net>
2023-05-18 14:33:34 +1000

    Add a backport helper script
    This helps speed up backporting sets of commits to multiple branches,
    but doesn't do much more than running git commands manually would.

---
+++ scripts/backport-commits.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+declare -a commits
+declare -a branches
+
+while [ "$1" != "--" ]
+do
+    branches+=("$1")
+    shift
+done
+
+shift
+
+while [ $# -gt 1 ]
+do
+    commits+=("$1")
+    shift
+done
+
+for branch in "${branches[@]}"
+do
+    git checkout "$branch" || exit 1
+    git cherry-pick -x "${commits[@]}" || $SHELL
+done
