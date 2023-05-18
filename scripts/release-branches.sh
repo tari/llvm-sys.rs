2dde98562df73f54241cfacb0d6aac967f67e483
Peter Marheine <peter@taricorp.net>
2023-05-18 14:41:58 +1000

    Add a script to help release multiple branches
    Basically just iterating over branches and running cargo-release.

---
+++ scripts/release-branches.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# release-branches.sh llvm-15.0 llvm-16.0 -- --features prefer-dynamic -- minor
+set -ex
+
+branches=()
+
+while [ "$1" != "--" ]
+do
+    branches+=("$1")
+    shift
+done
+shift
+
+for branch in "${branches[@]}"
+do
+    git checkout "$branch"
+    cargo release --no-dev-version "$@"
+done
